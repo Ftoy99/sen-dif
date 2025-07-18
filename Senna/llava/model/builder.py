a4b755e677d29243d94814ee8baf996357ca471c
Ftoy <spit10000@yahoo.gr>
2025-07-18 12:05:26 +0300

    Device debug

--- Senna/llava/model/builder.py
+++ Senna/llava/model/builder.py
@@ -224,7 +224,7 @@ def load_senna_pretrained_model(model_path, model_base, model_name, load_8bit=Fa
         vision_tower = model.get_vision_tower()
         if not vision_tower.is_loaded:
             vision_tower.load_model()
-        vision_tower.to(device=device_map, dtype=torch.float16)
+        vision_tower.to(device=device, dtype=torch.float16)
         image_processor = vision_tower.image_processor
 
     if hasattr(model.config, "max_sequence_length"):
