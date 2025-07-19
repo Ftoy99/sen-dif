0010e170de4bfae017de108099ced123d3727f25
Ftoy <spit10000@yahoo.gr>
2025-07-19 14:43:12 +0300

    Debug Lora

--- Senna/llava/model/builder.py
+++ Senna/llava/model/builder.py
@@ -183,7 +183,7 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
 def load_senna_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto",
                                 device="cuda", use_flash_attn=False, **kwargs):
     kwargs = {"device_map": device_map, **kwargs}
-
+    print("LOADING MODEL IN BUILDER")
     if device != "cuda":
         kwargs['device_map'] = {"": device}
 
