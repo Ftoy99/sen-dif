df4dc8514cc2a9b6060bfe9ed87b4200e7167c53
Ftoy <spit10000@yahoo.gr>
2025-07-19 12:19:04 +0300

    Reduce Lora size

--- Senna/train_tools/train_senna_llava_lora.sh
+++ Senna/train_tools/train_senna_llava_lora.sh
@@ -19,7 +19,7 @@ ln -s /cluster_home/custom_data/data/Senna/checkpoints .
 
 deepspeed /mnt/jimmys/sen-dif/Senna/llava/senna/train_senna_llava_multi_img.py \
     --deepspeed /mnt/jimmys/sen-dif/Senna/llava/zero3.json \
-    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --lora_enable True --lora_r 64 --lora_alpha 128 --mm_projector_lr 2e-5 \
     --freeze_img_adapter False \
     --init_weight_img_adapter False \
     --model_name_or_path $MODEL_PATH \
